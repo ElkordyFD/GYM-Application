5ddf98397d51262f30a3a536e64533fc93460491
elkordy <elkordydeveloper2003@gmail.com>
2023-11-24 20:40:33 +0200

    abstract person class uploaded

---
+++ PersonClass.java
@@ -0,0 +1,8 @@
+public abstract class Person {
+    private int id;
+    private String name;
+    private Boolean gender;
+    private String address;
+    private String phoneNumber;
+    private String email;
+}
