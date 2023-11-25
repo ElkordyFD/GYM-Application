314c25b2dd938f16a756bfd42acbb360eb848f68
ibrahimlinux03 <ebrahem.ehsan.saleh@gmail.com>
2023-11-25 18:05:00 +0200

    modified Customer Class

--- Customer.java
+++ Customer.java
@@ -1,10 +1,7 @@
 import java.util.*;
 
-public class Customer extends Person{
-    InBody []inBodies={};
-    Subscription subscription;
-    public Customer(int id, String name,Boolean gender,String address,String phoneNumber,String email) {
-        super(id,name,gender,address,phoneNumber,email);
-
-    }
+public class Customer extends Person {
+   private InBody[] inBodies = {};
+   private Subscription subscription;
 }
+
