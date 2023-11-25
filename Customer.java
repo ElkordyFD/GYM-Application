991d6956108f7b2aaa67beefe49a07394804ddf4
ibrahimlinux03 <ebrahem.ehsan.saleh@gmail.com>
2023-11-25 07:01:21 +0200

    Customer class uploaded

--- Customer.java
+++ Customer.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Customer {
+import java.util.Date;
+
+public class Customer extends Person{
+    public Customer(int id, String name,Boolean gender,String address,String phoneNumber,String email) {
+        super(id,name,gender,address,phoneNumber,email);
+
+    }
 }
--- Person.java
+++ Person.java
@@ -5,4 +5,13 @@ public abstract class Person {
     private String address;
     private String phoneNumber;
     private String email;
+
+    public Person(int id, String name, Boolean gender, String address, String phoneNumber, String email) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 }
