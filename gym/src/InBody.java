d25aadda0104d5cc5196c951abf18d5d7f697825
elkordy <elkordydeveloper2003@gmail.com>
2023-11-26 10:55:21 +0200

    updated

--- gym/src/InBody.java
+++ gym/src/InBody.java
@@ -1,11 +1,14 @@
 import java.util.*;
 public class InBody {
-    private final Date dateOfInBody=new Date();
+    private final Date DATE_OF_INBODY;
     private float height;
     private float totalWeight;
     private float bodyFatMass;
     private float minerals;
     private float totalBodyWater;
     private float protein;
+    public InBody() {
+        DATE_OF_INBODY=new Date();
+    }
 
 }
