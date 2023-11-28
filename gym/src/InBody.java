08633005ba9b94fc923c149cf6529b66fb8f8840
elkordy <elkordydeveloper2003@gmail.com>
2023-11-29 00:04:51 +0200

    updated

--- gym/src/InBody.java
+++ gym/src/InBody.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.time.LocalDate;
 public class InBody {
-    private int id;
     private LocalDate DATE_OF_INBODY;
     private float height;
     private float totalWeight;
