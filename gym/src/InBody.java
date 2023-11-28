ae693470eedaca4d6b488f458a68526f1660041d
elkordy <elkordydeveloper2003@gmail.com>
2023-11-28 23:56:29 +0200

    updated

--- gym/src/InBody.java
+++ gym/src/InBody.java
@@ -1,14 +1,33 @@
 import java.util.*;
+import java.time.LocalDate;
 public class InBody {
-    private final Date DATE_OF_INBODY;
+    private int id;
+    private LocalDate DATE_OF_INBODY;
     private float height;
     private float totalWeight;
     private float bodyFatMass;
     private float minerals;
     private float totalBodyWater;
     private float protein;
-    public InBody() {
-        DATE_OF_INBODY=new Date();
+    public InBody(LocalDate date) {
+
+        this.DATE_OF_INBODY=date;
     }
 
+    public LocalDate getDATE_OF_INBODY() {
+        return DATE_OF_INBODY;
+    }
+
+    @Override
+    public String toString() {
+        return "InBody{" +
+                "DATE_OF_INBODY=" + DATE_OF_INBODY +
+                ", height=" + height +
+                ", totalWeight=" + totalWeight +
+                ", bodyFatMass=" + bodyFatMass +
+                ", minerals=" + minerals +
+                ", totalBodyWater=" + totalBodyWater +
+                ", protein=" + protein +
+                '}';
+    }
 }
