2d41cc7737ddff78aafb96a2932ac15ae0bafec9
Yahya Hamza <112743393+Yahya2511@users.noreply.github.com>
2023-11-29 07:55:36 +0200

    Added getter and setter to  WorkingHours Variable!

--- gym/src/Coach.java
+++ gym/src/Coach.java
@@ -1,3 +1,11 @@
 public class Coach extends Person{
     private int WorkingHours;
+
+    public int getWorkingHours() {
+        return WorkingHours;
+    }
+
+    public void setWorkingHours(int workingHours) {
+        WorkingHours = workingHours;
+    }
 }
