aa675e3fd53e0e644a281a537ad0b23816abb914
elkordy <elkordydeveloper2003@gmail.com>
2023-12-01 18:20:33 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,6 @@ import java.util.*;
 import java.lang.*;
 public class Main {
     public static void main(String[] args) throws Exception {
-        File file;
-        file = new File("E:/Files/GYM.java");
-        GYM gym=(GYM) (FileReading.read(file));
-        file = new File("E:/Files/Customer.java");
-        ArrayList<Customer> customers =(ArrayList<Customer>) FileReading.read(file);
+
         }
     }
